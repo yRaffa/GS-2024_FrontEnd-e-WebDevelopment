ea3488b48e74a91035dbcd88f90901ae0fbfbf6f
Rafael Oliveira <yraffa.olive@gmail.com>
2024-06-06 22:03:32 -0300

    Update

--- src/javascript/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('#mobile-botao').on('click', function() {
-        $('#mobile-navbar').toggleClass('active');
-        $('#mobile-botao').find('i').toggleClass('fa-x');
-    });
-});
---
+++ src/javascript/scripts.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const mobileButton = document.getElementById("mobile-botao");
+    const mobileNavbar = document.getElementById("mobile-navbar");
+    const mobileButtonIcon = mobileButton.querySelector("i");
+  
+    mobileButton.addEventListener("click", function() {
+      mobileNavbar.classList.toggle("active");
+      mobileButtonIcon.classList.toggle("fa-x");
+    });
+  
+    const sections = document.querySelectorAll("section");
+    const navItems = document.querySelectorAll(".nav-item");
+  
+    window.addEventListener("scroll", function() {
+      const header = document.querySelector("header");
+      const scrollPosition = window.scrollY - header.offsetHeight;
+  
+      let activeSectionIndex = 0;
+  
+      if (scrollPosition <= 0) {
+        header.style.boxShadow = "none";
+      } else {
+        header.style.boxShadow = "5px 1px 5px rgba(0, 0, 0, 0.1)";
+      }
+  
+      sections.forEach(function(section, i) {
+        const sectionTop = section.offsetTop - 96;
+        const sectionBottom = sectionTop + section.offsetHeight;
+  
+        if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+          activeSectionIndex = i;
+          return;
+        }
+      });
+  
+      navItems.forEach(function(navItem) {
+        navItem.classList.remove("active");
+      });
+      navItems[activeSectionIndex].classList.add("active");
+    });
+  });
